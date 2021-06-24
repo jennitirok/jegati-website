77b81cfcb63d60c1701f8c43c2569e895330736e
Jennifer Gabriela Tirok <jennifer_tirok@yahoo.com>
2021-06-24 12:40:54 +0700

    Added Title to Each Page

--- website/views.py
+++ website/views.py
@@ -10,19 +10,19 @@ def home():
 # About Us Page
 @views.route('/about-us')
 def about_us():
-    return render_template("about_us.html")
+    return render_template("about_us.html", title="Tentang Kami")
 
 # Products Page
 @views.route('/products')
 def products():
-    return render_template("products.html")
+    return render_template("products.html", title="Produk")
 
 # Services Page
 @views.route('/services')
 def services():
-    return render_template("services.html")
+    return render_template("services.html", title="Servis")
 
 # Contact Us Page
 @views.route('/contact-us')
 def contact_us():
-    return render_template("contact_us.html")
+    return render_template("contact_us.html", title="Hubungi Kami")
